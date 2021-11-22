8bbd6a183b067f9c2b3e0cb777948ae260be0e7c
MiK <MiK@NB-KUBAT>
2021-11-22 10:31:55 +0100

    zkouska

---
+++ CNET2/Piskoviste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Piskoviste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
